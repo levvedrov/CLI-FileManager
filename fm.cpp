8b91a9b2a99f101c7ee510e4c678abf6331f65fd
Lev Vedrov <213868271+levvedrov@users.noreply.github.com>
2025-06-01 03:13:36 +0300

    main cmds are added

--- fm.cpp
+++ fm.cpp
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include <fstream>
 #include <unordered_map>
+#include <sstream>
+#include <string>
 #include <functional>
 namespace fs = std::filesystem;
 using namespace std;
@@ -44,11 +46,10 @@ class FileManager{
             fs::remove_all(path);
         }
         void readFile(string filename){
-            ifstream file(this->currentDir + filename);
+            ifstream file(this->currentDir + "/" + filename);
             string line;
-            while (!EOF)
+            while (getline(file, line))
             {
-                getline(file, line);
                 cout<<line<<endl;
             }
             file.close();
@@ -64,20 +65,127 @@ class FileManager{
 
 };
 
+tuple<int, vector<string>> parsLine(string line){
+    int cmdi=-1, index=0;
+    vector<string> param;
+    string word;
+    istringstream linestream(line);
 
+    while (linestream >> word){
+        if(index == 0){
+            if (word == "ls"){ cmdi = 0; }
+            if (word == "cd"){ cmdi = 1; }
+            if (word == "mkdir"){ cmdi = 2; }
+            if (word == "touch"){ cmdi = 3; }
+            if (word == "rm"){ cmdi = 4; }
+            if (word == "exit"){ cmdi = 5; }
+            if (word == "pwd"){ cmdi = 6; }
+            if (word == "cat"){ cmdi = 7; }
+            if (word == "write"){ cmdi = 8; }
+            if (word == "rmdir"){ cmdi = 9; }
+            
+        }
+        else{ 
+            param.push_back(word); 
+            }
+        
+        index++;
+    }
+
+    return {cmdi, param};
+}
+
+void displayError(string msg){
+    cout << "\033[31m[ ! ] " << msg << "\033[0m"<<endl;
+}
+
+void displaySuccsess(string msg){
+    cout << "\033[32m[ * ] " << msg << "\033[0m"<<endl;
+}
 
 int main (){
 
-    FileManager a("/Users/levvedrov/Documents/GitHub/FileManager/");
+    FileManager a("/Users/levvedrov/Documents/GitHub/CLI-FileManager");
     cout << """\033[31m\n .d8888b.  888      8888888\nd88P  Y88b 888        888\n888    888 888        888\n888        888        888\n888        888        888\n888    888 888        888\nY88b  d88P 888        888\n'Y8888P'  88888888 8888888\n  By Lev Vedrov\n\n\033[0m""";
-    string cmd;
+    string line;
 
 
-    while(cmd!="out"){
-        cout << "\033[0m" << ">" << a.getCurrentDir() << "\033[0m" << "> ";
-        cin >> cmd;
-        
+    while(line!="exit"){
+        cout << 	"\033[96m" << "" << a.getCurrentDir() << "\033[0m" << " >> " << "\033[0m";
+        getline(cin, line);
+        auto [cmd, par] = parsLine(line);
+        switch (cmd)
+        {
+        case 0:
+            a.listDir();
+        break;
 
+        case 1:
+            if (par.size() > 1) { displayError("cd : Too many arguments"); }
+            else{
+                if (par.empty()){ cout << "NotYet"<<endl; }
+                else{ a.changeDir(par[0]); }
+            }
+        break;
+
+        case 2:
+            if (par.size() > 1) { displayError("mkdir : Too many arguments"); }
+            else{
+                if (par.empty()){ displayError("mkdir : Need an argument"); }
+                else{
+                    a.createDir(par[0]);
+                    a.changeDir(a.getCurrentDir()+ "/" + par[0]); 
+                }
+            }
+        break;
+        case 3:
+            if (par.size() > 1) { displayError("touch : Too many arguments"); }
+            else{
+                if (par.empty()){ displayError("touch : Need an argument"); }
+                else{
+                    a.createFile(par[0]);
+                }
+            }
+            break;
+        case 4:
+            if (par.size() > 1) { displayError("rm : Too many arguments"); }
+            else{
+                if (par.empty()){ displayError("rm : Need an argument"); }
+                else{
+                    a.deleteFile(par[0]);
+                    
+                }
+            }
+            break;
+        case 5:
+            // exit
+            break;
+        case 6:
+             // pwd
+            break;
+        case 7:
+            if (par.size() > 1) { displayError("cat : Too many arguments"); }
+                else{
+                    if (par.empty()){ displayError("cat : Need an argument"); }
+                    else{
+                        a.readFile(par[0]);
+                    }
+                }
+            break;
+        case 8:
+            // write
+            break;
+        case 9:
+        if (par.size() > 1) { displayError("rmdir : Too many arguments"); }
+        else{
+            if (par.empty()){ displayError("rmdir : Need an argument"); }
+            else{
+                a.deleteDir(par[0]);
+            }
+        }
+            break;
+    }
+    
     }
     return 0;
 }
